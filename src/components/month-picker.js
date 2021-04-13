9dc6b7494fa4318ea8647a6cc4b7df6abeb79343
mahdihrs <mahdi.haris@myboost.id>
2021-04-13 17:18:14 +0700

    created month picker;

---
+++ src/components/month-picker.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { useQueryParams, StringParam } from 'use-query-params';
+import dayjs from 'dayjs';
+import { DatePicker } from 'antd';
+
+import { queryMonthFormatter } from '../utils/helpers/common';
+
+const { RangePicker } = DatePicker;
+
+function MonthPicker() {
+  const [query, setQuery] = useQueryParams({
+    revStart: StringParam,
+    revEnd: StringParam
+});
+
+  const onChange = ([startDate, endDate]) => {
+    setQuery({
+      revStart: queryMonthFormatter(startDate),
+      revEnd: queryMonthFormatter(endDate)
+    });
+  };
+
+  return (
+    <RangePicker
+      picker="month"
+      format="MMM-YYYY"
+      onChange={onChange}
+      value={[dayjs(query.revStart), dayjs(query.revEnd)]}
+    />
+  );
+}
+
+export default MonthPicker;
